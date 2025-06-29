576c0d853883fcc2ea1ffc71b43ec6b7396efb11
shwet91-cyber <shwetraj248@gmail.com>
2025-06-29 18:16:38 +0530

    Add files via upload
    enjoy and test this amazing tool .

---
+++ encryption.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+string encryption(string password,int key,int digit=0){
+    string encryptdata="";
+    if(digit==0)
+    digit=key;
+    for(auto chr:password){
+        if('A'<=chr && 'Z'>=chr)
+        encryptdata+=(65+(chr-65+key)%26);
+        else if('a'<=chr && 'z'>=chr)
+        encryptdata+=(97+(chr-97+key)%26);
+        else if('0'<=chr && '9'>=chr)
+        encryptdata+=('0'+(chr-'0'+digit)%10);
+        else
+        encryptdata+=chr;
+
+    }
+    return encryptdata;
+}
+string decryption(string password,int key){
+    return encryption(password,26-key,10-key);
+}
+int main(){ 
+    string password="Abc@123";
+    cout<<password<<endl;
+    string data=encryption(password,3);
+    cout<<data<<endl;
+
+   cout<< decryption(data,3);
+   cout<<endl;
+    return 0;
+}
