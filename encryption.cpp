cfcbd21406dc23945a2b8469bb322fb8f4bcb61a
shwet91-cyber <shwetraj248@gmail.com>
2025-06-29 18:43:17 +0530

    Update encryption.cpp

--- encryption.cpp
+++ encryption.cpp
@@ -1,10 +1,14 @@
 #include<iostream>
 #include<string.h>
 using namespace std;
-string encryption(string password,int key,int digit=0){
+string encryption(string password,int key,int digit=0,int key1=0,int key2=0){
     string encryptdata="";
     if(digit==0)
     digit=key;
+    if(key1==0)
+    key1=key;
+    if(key2==0)
+    key2=key;
     for(auto chr:password){
         if('A'<=chr && 'Z'>=chr)
         encryptdata+=(65+(chr-65+key)%26);
@@ -12,14 +16,20 @@ string encryption(string password,int key,int digit=0){
         encryptdata+=(97+(chr-97+key)%26);
         else if('0'<=chr && '9'>=chr)
         encryptdata+=('0'+(chr-'0'+digit)%10);
+        else{
+        if('!'<=chr && '/'>=chr)
+        encryptdata+=(33+(chr-33+key1)%15);
+        else if(':'<=chr && '@'>=chr)
+        encryptdata+=(58+(chr-58+key2)%7);
         else
         encryptdata+=chr;
+        }
 
     }
     return encryptdata;
 }
 string decryption(string password,int key){
-    return encryption(password,26-key,10-key);
+    return encryption(password,26-key,10-key,15-key,7-key);
 }
 int main(){ 
     string password="Abc@123";
